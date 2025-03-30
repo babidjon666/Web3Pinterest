c37f4ed237acc334a806e7a40b71986561b3deac
MarkPolyakov <markpolya@icloud.com>
2025-03-31 02:06:38 +0500

    ??

--- Server/obj/Debug/net9.0/Server.AssemblyInfo.cs
+++ Server/obj/Debug/net9.0/Server.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+73bf1c7f09a0123a82ae7441b604521341b049c8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b65b01970c636fdb54e11594352c9c8c801e96e4")]
 [assembly: System.Reflection.AssemblyProductAttribute("Server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

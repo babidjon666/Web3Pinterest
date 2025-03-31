ab745f3496f0d78a4723354279322720b23ace60
MarkPolyakov <markpolya@icloud.com>
2025-03-31 16:22:01 +0500

    Добавил ключи к S3 хранилищу

--- Server/obj/Debug/net9.0/Server.AssemblyInfo.cs
+++ Server/obj/Debug/net9.0/Server.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b0037a404695f13e586688f5e42a7d5851fe1d2b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+35ce0f92d2c7f6a183b3e3f00aa3d50e09d96786")]
 [assembly: System.Reflection.AssemblyProductAttribute("Server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
